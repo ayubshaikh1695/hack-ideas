41395d51e19a7f610a7aa1ccf90921548c9d5ef0
ayubshaikh1695 <ayubshaikh1695@gmail.com>
2021-09-01 15:01:27 +0530

    wrapped app component with auth context provider and added root-routes component as children and updated css for it

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import React from 'react';
+import RootRoutes from 'components/RootRoutes/root-routes.component';
+import AuthContextProvider from 'context/auth-context-provider';
 import './App.css';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <AuthContextProvider>
+      <div className='app-wrapper'>
+        <RootRoutes />
+      </div>
+    </AuthContextProvider>
+  );
 }
 
 export default App;
