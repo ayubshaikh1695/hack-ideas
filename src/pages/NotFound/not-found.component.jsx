81fc66c4b36db2bcd42152d9f228c05522b4c97b
ayubshaikh1695 <ayubshaikh1695@gmail.com>
2021-09-06 10:24:34 +0530

    updated not found page component

--- src/pages/NotFound/not-found.component.jsx
+++ src/pages/NotFound/not-found.component.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import styles from './not-found.module.css';
 
 function NotFound() {
-  return <div>Not Found</div>;
+  return (
+    <div className={styles.wrapper}>
+      <p className='h2 text-center'>404. Page Not Found</p>
+    </div>
+  );
 }
 
 export default NotFound;
