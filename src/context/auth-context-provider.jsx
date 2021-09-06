0216e26a7d2b88a25735aa17fda0212044bc6765
ayubshaikh1695 <ayubshaikh1695@gmail.com>
2021-09-06 10:24:00 +0530

    updated auth context provider with functions to set auth and clear auth

--- src/context/auth-context-provider.jsx
+++ src/context/auth-context-provider.jsx
@@ -1,4 +1,10 @@
-import React, { createContext, useState, useContext } from 'react';
+import React, {
+  createContext,
+  useState,
+  useContext,
+  useLayoutEffect,
+} from 'react';
+import { get } from 'services/api';
 
 const AuthContext = createContext();
 
@@ -6,14 +12,41 @@ export function useAuth() {
   return useContext(AuthContext);
 }
 
+// modify if new properties are added in the api response
+const defaultUserData = { id: null, name: '', upvotes: [] };
+
 function AuthContextProvider({ children }) {
   const [user, setUser] = useState({
     isAuthenticated: false,
-    id: null,
+    data: { ...defaultUserData },
   });
 
-  const setUserAuth = (userId, cb) => {
-    setUser({ isAuthenticated: true, id: userId });
+  useLayoutEffect(() => {
+    const user = JSON.parse(localStorage.getItem('user'));
+
+    if (user?.data?.id) {
+      (async () => {
+        const res = await get('/employees/' + user.data.id);
+
+        if (res.ok) {
+          setUserAuth(res.data);
+        } else {
+          removeUserAuth(() => {
+            window.history.pushState({}, '', '/login');
+          });
+        }
+      })();
+    }
+  }, []);
+
+  const setUserAuth = (userData, cb) => {
+    const user = {
+      isAuthenticated: true,
+      data: { ...userData },
+    };
+
+    setUser(user);
+    localStorage.setItem('user', JSON.stringify(user));
 
     if (cb) {
       setTimeout(cb, 100);
@@ -21,7 +54,12 @@ function AuthContextProvider({ children }) {
   };
 
   const removeUserAuth = (cb) => {
-    setUser({ isAuthenticated: false, id: null });
+    setUser({
+      isAuthenticated: false,
+      data: { ...defaultUserData },
+    });
+
+    localStorage.removeItem('user');
 
     if (cb) {
       setTimeout(cb, 100);
